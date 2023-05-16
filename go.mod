52441418621e864d9de7aa3101d165bf250cdc82
Rassim Medkour <racimmedkour@gmail.com>
2023-05-16 23:32:33 +0100

    JWT Main Base Code

--- go.mod
+++ go.mod
@@ -1,29 +1,25 @@
 module auth-service
 
-go 1.19
+go 1.20
 
 require (
-	github.com/ethereum/go-ethereum v1.11.5
-	github.com/labstack/echo/v4 v4.10.2
-	golang.org/x/crypto v0.6.0
-	gorm.io/driver/postgres v1.5.0
-	gorm.io/gorm v1.24.7-0.20230306060331-85eaf9eeda11
+	github.com/go-chi/chi/v5 v5.0.8
+	github.com/go-chi/jwtauth/v5 v5.1.0
+	github.com/rs/zerolog v1.29.1
 )
 
 require (
-	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
-	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
-	github.com/jackc/pgx/v5 v5.3.0 // indirect
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/labstack/gommon v0.4.0 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.17 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/net v0.7.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
-	golang.org/x/text v0.7.0 // indirect
-	golang.org/x/time v0.3.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
+	github.com/goccy/go-json v0.9.11 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.1 // indirect
+	github.com/lestrrat-go/httpcc v1.0.1 // indirect
+	github.com/lestrrat-go/httprc v1.0.4 // indirect
+	github.com/lestrrat-go/iter v1.0.2 // indirect
+	github.com/lestrrat-go/jwx/v2 v2.0.6 // indirect
+	github.com/lestrrat-go/option v1.0.0 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/rs/xid v1.4.0 // indirect
+	golang.org/x/crypto v0.0.0-20220427172511-eb4f295cb31f // indirect
+	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
 )
--- main.go
+++ main.go
@@ -1,202 +1,69 @@
 package main
 
 import (
-	"crypto/hmac"
-	"crypto/rand"
-	"encoding/base64"
-	"encoding/hex"
-	"errors"
 	"fmt"
 	"net/http"
+	"os"
 
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-	"golang.org/x/crypto/scrypt"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/jwtauth/v5"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/hlog"
 )
 
-type Password struct {
-	gorm.Model
-	Username string
-	Salt     string
-	Hash     string
-}
+var tokenAuth *jwtauth.JWTAuth
 
-func hashPassword(hashFunc func(password, salt []byte, N, r, p, keyLen int) ([]byte, error), dbFunc func(username string, salt string, hash string) error) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		// Parse the request body
-		type passwordRequest struct {
-			Username string `json:"username"`
-			Password string `json:"password"`
-		}
-		var req passwordRequest
-		if err := c.Bind(&req); err != nil {
-			return c.String(http.StatusBadRequest, "invalid request")
-		}
+func init() {
+	tokenAuth = jwtauth.New("HS256", []byte("secret"), nil)
 
-		// Generate a random salt
-		salt := make([]byte, 16)
-		if _, err := rand.Read(salt); err != nil {
-			return c.String(http.StatusInternalServerError, "failed to generate salt")
-		}
-		saltHex := hexutil.Encode(salt)
-
-		// Hash the password using scrypt
-		hash, err := hashFunc([]byte(req.Password), salt, 32768, 8, 1, 32)
-		if err != nil {
-			return c.String(http.StatusInternalServerError, "failed to hash password")
-		}
-		hashHex := hex.EncodeToString(hash)
-
-		// Store the salt and hash in the database
-		if err := dbFunc(req.Username, saltHex, hashHex); err != nil {
-			return c.String(http.StatusInternalServerError, "failed to save password")
-		}
-
-		// Return HTTP status OK
-		return c.String(http.StatusOK, "")
-	}
-}
-
-func saveToDB(username string, salt string, hash string) error {
-	db, err := gorm.Open(postgres.Open("host=localhost user=postgres dbname=postgres password=postgres sslmode=disable"), &gorm.Config{})
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-
-	defer func() {
-		dbSql, err := db.DB()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		dbSql.Close()
-	}()
-
-	p := Password{
-		Username: username,
-		Salt:     salt,
-		Hash:     hash,
-	}
-	if err := db.Create(&p).Error; err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func verifyPassword(hashFunc func(password, salt []byte, N, r, p, keyLen int) ([]byte, error), password string, saltHex string, hashHex string) bool {
-	salt, err := hexutil.Decode(saltHex)
-	if err != nil {
-		return false
-	}
-	hash, err := hex.DecodeString(hashHex)
-	if err != nil {
-		return false
-	}
-
-	computedHash, err := hashFunc([]byte(password), salt, 32768, 8, 1, 32)
-	if err != nil {
-		return false
-	}
-
-	return hmac.Equal(hash, computedHash)
-}
-
-func getPassword(db *gorm.DB, hashFunc func(password, salt []byte, N, r, p, keyLen int) ([]byte, error)) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		// Parse the request body
-		type passwordRequest struct {
-			Username string `json:"username"`
-			Password string `json:"password"`
-		}
-		var req passwordRequest
-		if err := c.Bind(&req); err != nil {
-			return c.String(http.StatusBadRequest, "invalid request")
-		}
-		// Get the password hash from the database
-		var p Password
-		if err := db.Where("username = ?", req.Username).First(&p).Error; err != nil {
-			if errors.Is(err, gorm.ErrRecordNotFound) {
-				return c.String(http.StatusNotFound, "username not found")
-			}
-			return c.String(http.StatusInternalServerError, "failed to get password")
-		}
-
-		// Verify the password hash
-		if !verifyPassword(hashFunc, req.Password, p.Salt, p.Hash) {
-			return c.String(http.StatusUnauthorized, "invalid password")
-		}
-
-		// Return HTTP status OK
-		return c.String(http.StatusOK, "")
-	}
+	// For debugging/example purposes, we generate and print
+	// a sample jwt token with claims `user_id:123` here:
+	_, tokenString, _ := tokenAuth.Encode(map[string]interface{}{"user_id": 123})
+	fmt.Printf("DEBUG: a sample jwt is %s\n\n", tokenString)
 }
 
 func main() {
-	e := echo.New()
+	addr := ":3333"
+	fmt.Printf("Starting server on %v\n", addr)
 
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
+	logger := zerolog.New(os.Stdout).With().Timestamp().Logger()
+	router := router(logger)
 
-	e.POST("/password", hashPassword(scrypt.Key, saveToDB))
-	e.GET("/password", getPasswordHandler(getPasswordFromDB))
+	loggedRouter := hlog.NewHandler(logger)(router)
 
-	e.Logger.Fatal(e.Start(":8080"))
-}
-
-func getPasswordHandler(getPassword func(string) (*Password, error)) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		username := c.QueryParam("username")
-		password := c.QueryParam("password")
-
-		p, err := getPassword(username)
-		if err != nil {
-			if errors.Is(err, gorm.ErrRecordNotFound) {
-				return c.String(http.StatusUnauthorized, "invalid username or password")
-			}
-			return c.String(http.StatusInternalServerError, "failed to query password")
-		}
-
-		salt, err := base64.StdEncoding.DecodeString(p.Salt)
-		if err != nil {
-			return c.String(http.StatusInternalServerError, "failed to decode salt")
-		}
-
-		hash, err := scrypt.Key([]byte(password), salt, 16384, 8, 1, 32)
-		if err != nil {
-			return c.String(http.StatusInternalServerError, "failed to hash password")
-		}
-
-		hashStr := base64.StdEncoding.EncodeToString(hash)
-
-		if hashStr != p.Hash {
-			return c.String(http.StatusUnauthorized, "invalid username or password")
-		}
-
-		return c.String(http.StatusOK, "login successful")
+	if err := http.ListenAndServe(addr, loggedRouter); err != nil {
+		logger.Fatal().Err(err).Msg("Server error")
 	}
 }
 
-func getPasswordFromDB(username string) (*Password, error) {
-	dsn := "host=localhost port=5432 user=postgres dbname=passwords password=password sslmode=disable"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		return nil, fmt.Errorf("failed to connect to database: %v", err)
-	}
-	sqlDB, err := db.DB()
-	if err != nil {
-		return nil, fmt.Errorf("failed to get db: %v", err)
-	}
-	defer sqlDB.Close()
+func router(logger zerolog.Logger) http.Handler {
+	r := chi.NewRouter()
 
-	var p Password
-	if err := db.Where("username = ?", username).First(&p).Error; err != nil {
-		return nil, err
-	}
+	// Protected routes
+	r.Group(func(r chi.Router) {
+		// Seek, verify and validate JWT tokens
+		r.Use(jwtauth.Verifier(tokenAuth))
 
-	return &p, nil
+		// Handle valid / invalid tokens. In this example, we use
+		// the provided authenticator middleware, but you can write your
+		// own very easily, look at the Authenticator method in jwtauth.go
+		// and tweak it, its not scary.
+		r.Use(jwtauth.Authenticator)
+
+		r.Get("/admin", func(w http.ResponseWriter, r *http.Request) {
+			_, claims, _ := jwtauth.FromContext(r.Context())
+			logger.Info().Msgf("Protected area. User ID: %v", claims["user_id"])
+			w.Write([]byte(fmt.Sprintf("protected area. hi %v", claims["user_id"])))
+		})
+	})
+
+	// Public routes
+	r.Group(func(r chi.Router) {
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			logger.Info().Msg("Welcome anonymous")
+			w.Write([]byte("welcome anonymous"))
+		})
+	})
+
+	return r
 }
