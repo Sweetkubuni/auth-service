32cd8ab3aa06c2bec2879fc7a73b08487e46d38c
luisya22 <lmatosgarcia2@gmail.com>
2023-08-23 17:16:30 -0400

    chore: add swagger

--- controllers/securecontroller.go
+++ controllers/securecontroller.go
@@ -6,6 +6,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Ping godoc
+//
+//	@Summary		Ping the secured endpoint
+//	@Description	Returns a pong message
+//	@Tags			utils
+//	@Produce		json
+//	@Security		ApiKeyAuth
+//	@Success		200	{object}	map[string]string	"Ping sucessful"
+//	@Router			/secured/ping [get]
 func (app *App) Ping(context *gin.Context) {
 	context.JSON(http.StatusOK, gin.H{"message": "pong"})
 }
--- controllers/tokencontroller.go
+++ controllers/tokencontroller.go
@@ -9,11 +9,34 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Token Request represents the credentials required to fetch a token.
+//
+// TokenRequest godoc
+//	@Schema		TokenRequest
+//	@Property	email (string) "User's email address" true
+//	@Property	password(string) "User's password" true
 type TokenRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
+type TokenResponse struct {
+	Token string `json:"token"`
+}
+
+// GenerateToken godoc
+//
+//	@Summary		Generate a token
+//	@Description	Generate a JWT token for a user given their email and password
+//	@Tags			auth
+//	@Accept			json
+//	@Produce		json
+//	@Param			body	body		TokenRequest		true	"Credentials for token generation"
+//	@Success		200		{object}	TokenResponse		"Token generated successfully"
+//	@Failure		400		{object}	map[string]string	"Bad request"
+//	@Failure		401		{object}	map[string]string	"Unauthorized"
+//	@Failure		500		{object map[string]string "Internal Server Error"
+//	@Router			/token [post]
 func (app *App) GenerateToken(context *gin.Context) {
 	var request TokenRequest
 	var user models.User
